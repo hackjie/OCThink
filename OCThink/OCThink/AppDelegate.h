312ef0f205afe571168247625225c6cc3fc76e9d
leoli <codelijie@gmail.com>
2018-08-15 16:42:13 +0800

    Init

---
+++ OCThink/OCThink/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OCThink
+//
+//  Created by leoli on 2018/8/15.
+//  Copyright © 2018 leoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OCThink/OCThink/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCThink
+//
+//  Created by leoli on 2018/8/15.
+//  Copyright © 2018 leoli. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
